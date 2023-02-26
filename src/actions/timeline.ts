390a21dff843a2b929a632898e00b6290caadd20
eternal <eternal.git@gmail.com>
2023-02-26 16:31:40 +0000

    Update timeline.ts

--- src/actions/timeline.ts
+++ src/actions/timeline.ts
@@ -1,9 +1,9 @@
 import type { UserTimelineV1Paginator } from 'twitter-api-v2';
 import Client from '../client';
 
-export default function (username: string) {
+export default async function (username: string) {
   try {
-    return Client.instance.v1.userTimelineByUsername(username, Client.config.timeline);
+    return await Client.instance.v1.userTimelineByUsername(username, Client.config.timeline);
   } catch {
     return { data: null } as UserTimelineV1Paginator;
   }
