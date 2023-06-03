aeb71076e30b2cb48dd640ba2543e80f32578574
eternal <eternal.git@gmail.com>
2023-06-03 18:12:01 +0100

    exit with failure code on uncaught exception

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,21 @@ import config from '../config.json';
 
 Client.connect();
 
-if (config.errors.catch) {
-  const webhook = new Webhook(config.errors.webhook);
+const webhook = new Webhook(config.errors?.webhook);
 
-  process.on('uncaughtException', (error, origin) => {
-    webhook.send({
-      content: [
-        '**An error occured inside discord-twitter-forward**',
-        '',
-        `Origin: \`${origin ?? 'Unknown'}\``,
-        `Cause: \`${error.cause ?? 'Unknown'}\``,
-        `Type: \`${error.name}\``,
-        `Stack: \`\`\`\n${error.stack}\n\`\`\``,
-      ].join('\n')
-    });
-  });
-}
+process.on('uncaughtException', (error, origin) => {
+	if (config.errors.catch) {
+		webhook.send({
+			content: [
+				'**An error occured inside discord-twitter-forward**',
+				'',
+				`Origin: \`${origin ?? 'Unknown'}\``,
+				`Cause: \`${error.cause ?? 'Unknown'}\``,
+				`Type: \`${error.name}\``,
+				`Stack: \`\`\`\n${error.stack}\n\`\`\``,
+			].join('\n')
+		});
+	};
+
+	process.exit(1);
+});
